bb70f227de99e7860d1e5e8c03076c4d50e42396
Addis <lumiaddis@gmail.com>
2024-08-15 17:42:30 -0400

    Sintaxis básica de php, variables, saltos de línea, concatenación, etc

---
+++ src/sintaxis-basica.php
@@ -0,0 +1,23 @@
+<?php
+
+// echo "Hola, mamá. Estoy programando con PHPCito \n";
+
+$nombre = "Carlos";
+$apellido = "Santana";
+
+// echo "No, no. Yo me llamo " . $nombre . "" . $apellido . "\n";
+// echo "No, no. Yo me llamo $nombre $apellido \n";
+// echo "El resultado de 4 x 5 es " . (4 * 5);
+
+$personas = [
+    "Carlos" => 22,
+    "Mr. Michi" => 15,
+    "Juan" => 65
+];
+// var_dump( $personas );
+print_r( $personas );
+
+?>
+
+<!-- \n salto de línea para la terminal
+<br> salto de línea para navegador -->
